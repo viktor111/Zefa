dff9becbdfb852df39cb11a4f4bb763f32a2138c
viktor111 <swifrorlilko@gmail.com>
2022-08-30 13:29:15 +0300

    Add Commin entity

---
+++ Zefa.Domain/Common/Entity.cs
@@ -0,0 +1,45 @@
+namespace Zefa.Domain.Common;
+
+/// <summary>
+///     The base class for all domain entities.
+/// </summary>
+/// <typeparam name="TId">
+///     The type of the identifier..
+/// </typeparam>
+public abstract class Entity<TId>
+    where TId : struct
+{
+    public TId Id { get; private set; } = default;
+
+    public override bool Equals(object? obj)
+    {
+        if (!(obj is Entity<TId> other)) return false;
+
+        if (ReferenceEquals(this, other)) return true;
+
+        if (GetType() != other.GetType()) return false;
+
+        if (Id.Equals(default) || other.Id.Equals(default)) return false;
+
+        return Id.Equals(other.Id);
+    }
+
+    public static bool operator ==(Entity<TId>? first, Entity<TId>? second)
+    {
+        if (first is null && second is null) return true;
+
+        if (first is null || second is null) return false;
+
+        return first.Equals(second);
+    }
+
+    public static bool operator !=(Entity<TId>? first, Entity<TId>? second)
+    {
+        return !(first == second);
+    }
+
+    public override int GetHashCode()
+    {
+        return (GetType().ToString() + Id).GetHashCode();
+    }
+}

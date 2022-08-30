c40a0d1185b3c79edd07b5c2c51230aaea7e49d5
viktor111 <swifrorlilko@gmail.com>
2022-08-30 13:56:28 +0300

    Add ValueObject

--- Zefa.Domain/Common/ValueObject.cs
+++ Zefa.Domain/Common/ValueObject.cs
@@ -1,6 +1,77 @@
+using System.Reflection;
+
 namespace Zefa.Domain.Common;
 
-public class ValueObject
+public abstract class ValueObject
 {
-    
+    private readonly BindingFlags privateBindingFlags =
+        BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+
+    public override bool Equals(object? other)
+    {
+        if (other == null) return false;
+
+        var type = GetType();
+        var otherType = other.GetType();
+
+        if (type != otherType) return false;
+
+        var fields = type.GetFields(privateBindingFlags);
+
+        foreach (var field in fields)
+        {
+            var firstValue = field.GetValue(other);
+            var secondValue = field.GetValue(this);
+
+            if (firstValue == null)
+            {
+                if (secondValue != null) return false;
+            }
+            else if (!firstValue.Equals(secondValue))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        var fields = GetFields();
+
+        const int startValue = 17;
+        const int multiplier = 59;
+
+        return fields
+            .Select(field => field.GetValue(this))
+            .Where(value => value != null)
+            .Aggregate(startValue, (current, value) => current * multiplier + value!.GetHashCode());
+    }
+
+    private IEnumerable<FieldInfo> GetFields()
+    {
+        var type = GetType();
+
+        var fields = new List<FieldInfo>();
+
+        while (type != typeof(object))
+        {
+            fields.AddRange(type.GetFields(privateBindingFlags));
+
+            type = type.BaseType!;
+        }
+
+        return fields;
+    }
+
+    public static bool operator ==(ValueObject first, ValueObject second)
+    {
+        return first.Equals(second);
+    }
+
+    public static bool operator !=(ValueObject first, ValueObject second)
+    {
+        return !(first == second);
+    }
 }

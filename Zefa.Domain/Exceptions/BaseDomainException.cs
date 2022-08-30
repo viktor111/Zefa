0f839d83932666059af3e5fcedd72437ebb92347
viktor111 <swifrorlilko@gmail.com>
2022-08-30 13:57:04 +0300

    Add Guard, BaseDomainException and ModelConstants

--- Zefa.Domain/Exceptions/BaseDomainException.cs
+++ Zefa.Domain/Exceptions/BaseDomainException.cs
@@ -1,6 +1,12 @@
 namespace Zefa.Domain.Exceptions;
 
-public class BaseDomainException
+public abstract class BaseDomainException : Exception
 {
-    
+    private string? error;
+
+    public string Error
+    {
+        get => error ?? base.Message;
+        set => error = value;
+    }
 }
--- Zefa.Domain/Models/ModelConstants.cs
+++ Zefa.Domain/Models/ModelConstants.cs
@@ -2,5 +2,14 @@ namespace Zefa.Domain.Models;
 
 public class ModelConstants
 {
-    
+    public class Common
+    {
+        public const int MinNameLength = 2;
+        public const int MaxNameLength = 20;
+        public const int MinEmailLength = 3;
+        public const int MaxEmailLength = 50;
+        public const int MaxUrlLength = 2048;
+        public const int Zero = 0;
+        public const string AdministratorRoleName = "Administrator";
+    }
 }
